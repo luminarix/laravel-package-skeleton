139bdda61fc11b05399eaf585e7c33cb32d6d1bc
Mark <14284867+xHeaven@users.noreply.github.com>
2024-04-19 19:00:44 +0200

    initial commit

---
+++ config/skeleton.php
@@ -0,0 +1,6 @@
+<?php
+
+// config for Luminarix/Skeleton
+return [
+
+];
---
+++ tests/TestCase.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Luminarix\Skeleton\Tests;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Orchestra\Testbench\TestCase as Orchestra;
+use Luminarix\Skeleton\SkeletonServiceProvider;
+
+class TestCase extends Orchestra
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Factory::guessFactoryNamesUsing(
+            fn (string $modelName) => 'Luminarix\\Skeleton\\Database\\Factories\\'.class_basename($modelName).'Factory'
+        );
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            SkeletonServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        config()->set('database.default', 'testing');
+
+        /*
+        $migration = include __DIR__.'/../database/migrations/create_skeleton_table.php.stub';
+        $migration->up();
+        */
+    }
+}
